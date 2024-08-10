ca3958999cc3d2c5aa8a8bf720f607dce631a647
Akarsh <akarshbalu1@gmail.com>
2024-08-10 20:43:48 +0530

    fixed solution only loading while clicked twice bug

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const load_btn_doc = document.getElementById("load_btn");
 
 let inp_arr = [];
 let inp_arr_cpy = [];
-let inp_arr_cpy_stor=[];
+let inp_arr_cpy_stor = [];
 let sol_arr = [];
 let get_sudo_arr = [];
 let get_sol_sudo_arr = [];
-let sol_state= "hide";
+let sol_state = "hide";
 let inp_arr_stor = [];
 let sol_arr_stor = [];
 const URL =
@@ -22,32 +22,28 @@ const URL =
 
 new_btn_doc.addEventListener("click", function () {
   location.reload();
+  localStorage.clear();
 });
-
 
 // solution
 
 for (let i = 0; i < 81; i++) {
-  
-    sol_arr[i] = document.createElement("input");
-    sol_arr[i].readOnly = true;
-    sol_arr[i].type = "text";
+  sol_arr[i] = document.createElement("input");
+  sol_arr[i].readOnly = true;
+  sol_arr[i].type = "text";
 
-    
-    sol_cont_doc.append(sol_arr[i])
+  sol_cont_doc.append(sol_arr[i]);
 }
 
-if(sol_state=="hide"){
-  sol_cont_doc.style.display="none";
-}
-else if(sol_state=="show"){
-  sol_cont_doc.style.display="grid";
+if (sol_state == "hide") {
+  sol_cont_doc.style.display = "none";
+} else if (sol_state == "show") {
+  sol_cont_doc.style.display = "grid";
 }
 
 for (let i = 0; i < 81; i++) {
   inp_arr[i] = document.createElement("input");
   inp_arr[i].type = "text";
-
 
   inp_arr[i].addEventListener("input", function () {
     if (
@@ -81,37 +77,33 @@ async function getSudo() {
   get_sudo_arr = [...data.newboard.grids[0].value];
   get_sol_sudo_arr = [...data.newboard.grids[0].solution];
 
-
-//  console.log(get_sol_sudo_arr);
+  //  console.log(get_sol_sudo_arr);
 
   diff_doc.innerHTML = `Difficulty: ${data.newboard.grids[0].difficulty}`;
-
-
 
   sub_btn_doc.addEventListener("click", function () {
     checkValues();
     chckInitialVal();
   });
 
-  sol_btn_doc.addEventListener("click",function(){
-    
+  sol_btn_doc.addEventListener("click", function () {
     showSolution();
-    putValuesSol();
-  })
+  });
 
-  save_btn_doc.addEventListener("click",function(){
-saveInpVal();
-saveCpyVal();
-saveSolVal();
-  })
+  save_btn_doc.addEventListener("click", function () {
+    saveInpVal();
+    saveCpyVal();
+    saveSolVal();
+  });
 
-  load_btn_doc.addEventListener("click",function(){
+  load_btn_doc.addEventListener("click", function () {
     loadInpVal();
     loadCpyVal();
     loadSolVal();
-  })
+  });
 
-  cpyInpVal()
+  cpyInpVal();
+  putValuesSol();
   putValuesSudo();
 }
 
@@ -151,13 +143,9 @@ function putValuesSudo() {
       inp_arr[i].value = "";
     }
   }
-
-  
-  
-
 }
 
-function cpyInpVal(){
+function cpyInpVal() {
   for (let w = 0; w < 9; w++) {
     inp_arr_cpy[w] = get_sudo_arr[0][w];
   }
@@ -193,21 +181,19 @@ function cpyInpVal(){
       inp_arr_cpy[i] = "";
     }
   }
-
 }
 
-function chckInitialVal(){
-for (let i = 0; i < 81; i++) {
-  if(inp_arr_cpy[i]==inp_arr[i].value){
-    inp_arr[i].style.color="black";
+function chckInitialVal() {
+  for (let i = 0; i < 81; i++) {
+    if (inp_arr_cpy[i] == inp_arr[i].value) {
+      inp_arr[i].style.color = "black";
+    }
   }
-}
-  
 }
 
 function checkValues() {
   for (let w = 0; w < 9; w++) {
-    if (inp_arr[w].value != get_sol_sudo_arr[0][w]) {
+    if (inp_arr[w].value != sol_arr[w].value) {
       inp_arr[w].style.color = "red";
     } else {
       inp_arr[w].style.color = "green";
@@ -215,7 +201,7 @@ function checkValues() {
   }
 
   for (let w = 9; w < 18; w++) {
-    if (inp_arr[w].value != get_sol_sudo_arr[1][w - 9]) {
+    if (inp_arr[w].value != sol_arr[w].value) {
       inp_arr[w].style.color = "red";
     } else {
       inp_arr[w].style.color = "green";
@@ -223,49 +209,51 @@ function checkValues() {
   }
 
   for (let w = 18; w < 27; w++) {
-    if (inp_arr[w].value != get_sol_sudo_arr[2][w - 18]) {
+    if (inp_arr[w].value != sol_arr[w].value) {
       inp_arr[w].style.color = "red";
     } else {
       inp_arr[w].style.color = "green";
     }
   }
   for (let w = 27; w < 36; w++) {
-    if (inp_arr[w].value != get_sol_sudo_arr[3][w - 27]) {
+    if (inp_arr[w].value != sol_arr[w].value) {
       inp_arr[w].style.color = "red";
     } else {
       inp_arr[w].style.color = "green";
     }
   }
   for (let w = 36; w < 45; w++) {
-    if (inp_arr[w].value != get_sol_sudo_arr[4][w - 36]) {
+    if (inp_arr[w].value != sol_arr[w].value) {
       inp_arr[w].style.color = "red";
     } else {
       inp_arr[w].style.color = "green";
     }
   }
   for (let w = 45; w < 54; w++) {
-    if (inp_arr[w].value != get_sol_sudo_arr[5][w - 45]) {
+    if (inp_arr[w].value != sol_arr[w].value) {
       inp_arr[w].style.color = "red";
     } else {
       inp_arr[w].style.color = "green";
     }
   }
+   
+  
   for (let w = 54; w < 63; w++) {
-    if (inp_arr[w].value != get_sol_sudo_arr[6][w - 54]) {
+    if (inp_arr[w].value != sol_arr[w].value) {
       inp_arr[w].style.color = "red";
     } else {
       inp_arr[w].style.color = "green";
     }
   }
   for (let w = 63; w < 72; w++) {
-    if (inp_arr[w].value != get_sol_sudo_arr[7][w - 63]) {
+    if (inp_arr[w].value != sol_arr[w].value) {
       inp_arr[w].style.color = "red";
     } else {
       inp_arr[w].style.color = "green";
     }
   }
   for (let w = 72; w < 81; w++) {
-    if (inp_arr[w].value != get_sol_sudo_arr[8][w - 72]) {
+    if (inp_arr[w].value != sol_arr[w].value) {
       inp_arr[w].style.color = "red";
     } else {
       inp_arr[w].style.color = "green";
@@ -273,23 +261,19 @@ function checkValues() {
   }
 }
 
-
 function showSolution() {
-if(sol_state=="hide"){
-  sol_state="show";
-  sol_btn_doc.innerHTML=`hide solution`;
-  sol_cont_doc.style.display="grid"
-}
-else if(sol_state=="show"){
-  sol_state="hide";
-  sol_btn_doc.innerHTML=`view solution`;
-  sol_cont_doc.style.display="none"
-
-
-}
+  if (sol_state == "hide") {
+    sol_state = "show";
+    sol_btn_doc.innerHTML = `hide solution`;
+    sol_cont_doc.style.display = "grid";
+  } else if (sol_state == "show") {
+    sol_state = "hide";
+    sol_btn_doc.innerHTML = `view solution`;
+    sol_cont_doc.style.display = "none";
+  }
 }
 
-function putValuesSol(){
+function putValuesSol() {
   for (let w = 0; w < 9; w++) {
     sol_arr[w].value = get_sol_sudo_arr[0][w];
   }
@@ -321,46 +305,80 @@ function putValuesSol(){
   }
 }
 
-function loadInpVal(){
+function loadInpVal() {
   for (let i = 0; i < 81; i++) {
-    try{
-    inp_arr[i].value = JSON.parse(localStorage.getItem("inparr"))[i]
+    try {
+      inp_arr[i].value = JSON.parse(localStorage.getItem("inparr"))[i];
+    } catch {
+      console.log("not able to load");
     }
-    catch{
-      console.log("not able to load")
-    }
-
-}
-}
-function loadSolVal(){
-  for (let i = 0; i < 81; i++) {
-    sol_arr[i].value=JSON.parse(localStorage.getItem("solarr"))[i]
   }
-
 }
- function loadCpyVal(){
+function loadSolVal() {
+  let temp_sol_arr = JSON.parse(localStorage.getItem("solarr"));
+
+  for (let i = 0; i < 81; i++) {
+    sol_arr[i].value = temp_sol_arr[i];
+  }
+}
+function loadCpyVal() {
   for (let i = 0; i < 81; i++) {
     inp_arr_cpy[i] = JSON.parse(localStorage.getItem("inparrcpy"))[i];
   }
-} 
-function saveInpVal(){
-for (let i = 0; i < 81; i++) {
-  inp_arr_stor[i]=inp_arr[i].value;
-  
 }
-localStorage.setItem("inparr",JSON.stringify(inp_arr_stor));
-}
-function saveSolVal(){
-for (let i = 0; i < 81; i++) {
-  sol_arr_stor[i]=sol_arr[i].value;
-}
-localStorage.setItem("solarr",JSON.stringify(sol_arr_stor));
-
-}
-function saveCpyVal(){
+function saveInpVal() {
   for (let i = 0; i < 81; i++) {
-    inp_arr_cpy_stor[i]=inp_arr_cpy[i];
+    inp_arr_stor[i] = inp_arr[i].value;
   }
-  localStorage.setItem("inparrcpy",JSON.stringify(inp_arr_cpy_stor));
+  localStorage.setItem("inparr", JSON.stringify(inp_arr_stor));
 }
+function saveSolVal() {
+  for (let i = 0; i < 81; i++) {
+    sol_arr_stor[i] = sol_arr[i].value;
+    //sol_arr_stor[i]=get_sol_sudo_arr[i]
+  }
+  for (let w = 0; w < 9; w++) {
+    sol_arr_stor[w] = get_sol_sudo_arr[0][w];
+  }
+
+  for (let w = 9; w < 18; w++) {
+    sol_arr_stor[w] = get_sol_sudo_arr[1][w - 9];
+  }
+
+  for (let w = 18; w < 27; w++) {
+    sol_arr_stor[w] = get_sol_sudo_arr[2][w - 18];
+  }
+  for (let w = 27; w < 36; w++) {
+    sol_arr_stor[w] = get_sol_sudo_arr[3][w - 27];
+  }
+  for (let w = 36; w < 45; w++) {
+    sol_arr_stor[w] = get_sol_sudo_arr[4][w - 36];
+  }
+  for (let w = 45; w < 54; w++) {
+    sol_arr_stor[w] = get_sol_sudo_arr[5][w - 45];
+  }
+  for (let w = 54; w < 63; w++) {
+    sol_arr_stor[w] = get_sol_sudo_arr[6][w - 54];
+  }
+  for (let w = 63; w < 72; w++) {
+    sol_arr_stor[w] = get_sol_sudo_arr[7][w - 63];
+  }
+  for (let w = 72; w < 81; w++) {
+    sol_arr_stor[w] = get_sol_sudo_arr[8][w - 72];
+  }
+  localStorage.setItem("solarr", JSON.stringify(sol_arr_stor));
+}
+function saveCpyVal() {
+  for (let i = 0; i < 81; i++) {
+    inp_arr_cpy_stor[i] = inp_arr_cpy[i];
+  }
+  localStorage.setItem("inparrcpy", JSON.stringify(inp_arr_cpy_stor));
+}
+
 getSudo();
+
+//##############################################################################################
+
+//create sperate array for solution set and do rather than just directly taking from input.value
+
+//##############################################################################################
